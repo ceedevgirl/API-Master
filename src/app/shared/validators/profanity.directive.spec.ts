a83b319428066baad43c483807cf92d657444973
ceedevgirl <ceciliaarthur347@gmail.com>
2025-06-16 20:18:47 +0000

    Project setup

---
+++ src/app/shared/validators/profanity.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ProfanityDirective } from './profanity.directive';
+
+describe('ProfanityDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ProfanityDirective();
+    expect(directive).toBeTruthy();
+  });
+});
